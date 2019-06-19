1fd245fae26a255a0022792f8a9b322199dd3b56
Nishant Soni <nishant@npsoni.com>
2019-06-19 05:52:45 +0530

    docker provider for terraform

---
+++ Docker/main.tf
@@ -0,0 +1,18 @@
+#Download the latest ghost image
+resource "docker_image" "image_id"{
+  name = "ghost:latest"
+}
+#use "terraform show" to view the details of the image
+
+#Start the container
+resource "docker_container" "container_id"{
+  name = "blog"
+  image = "${docker_image.image_id.latest}"
+  ports {
+    internal = "2368"
+    external = "80"
+  }
+}
+
+#terraform plan
+#terraform apply (you can also make use of auto-approve)
